9225fcc23a805a87e44dbc1f69f8026b75ab7bf3
Dan Collins <daniel.t.collins@gmail.com>
2015-05-11 10:38:11 -0400

    Updating library to use the Google Custom Search JSON API

--- README.md
+++ README.md
@@ -1,14 +1,24 @@
-Google Site Search 
+Google Custom Search 
 ==================
 
-A PHP interface to the Google Custom Search XML API. 
-Google's documentation for the Custom Search XML API can be found here:
-https://developers.google.com/custom-search/docs/xml\_results
+A PHP interface to the Google Custom Search JSON API. The JSON API allows for [free](https://cse.google.com/cse) and [paid](https://www.google.com/work/search/products/gss.html) users to take advantage of adding Google search results to your website.
+
+Google's documentation for the Custom Search JSON API can be found here:
+https://developers.google.com/custom-search/json-api/v1/overview
+
+Credentials
+-----
+
+**Search Engine ID** - On the [Custom Search Engine Control Panel](http://www.google.com/cse/manage/all), create a new search engine for the site you would like to integrate. Once created, you can retrieve your Search Engine ID from the *Setup* tab.
+
+**API Key** - If you're using the free option, visit the [Google Developers Console](https://console.developers.google.com) and create a project for your search engine. Within your project, you’ll need to enable the *Custom Search API* from the *APIs* tab. Finally, on the *Credentials* tab, you will need to create a Public API access key by selecting the *Create new Key* option and choosing *Server key*. The API Key will now be available.
+
+If you're using a paid option, you can find your API key in the [Control Panel](http://www.google.com/cse/manage/all) in the *Business > XML & JSON* tab.
 
 Usage
 -----
 
 ```(php)
-$search = new iMarc\GoogleSiteSearch(MY_SITE_SEARCH_KEY);
-$results = $search->search('Bananas');
+$search = new iMarc\GoogleCustomSearch(SEARCH_ENGINE_ID, API_KEY);
+$results = $search->search('Apples');
 ```
---
+++ src/GoogleCustomSearch.php
@@ -0,0 +1,148 @@
+<?php
+namespace iMarc;
+
+/**
+ * Google Custom Search
+ *
+ * A PHP interface to the Google Custom Search JSON API
+ *
+ * Documentation for the Google Custom Search JSON API
+ * https://developers.google.com/custom-search/json-api/v1/overview
+ *
+ * Usage:
+ *
+ * $search = new iMarc\GoogleCustomSearch(SEARCH_ENGINE_ID, API_KEY);
+ * $results = $search->search('Apples');
+ *
+ * @copyright iMarc LLC 2015
+ * @author Jeff Turcotte <jeff@imarc.net>
+ * @author Dan Collins <dan@imarc.net>
+ * @version 2.0.0
+ * 
+ * @license MIT
+**/
+class GoogleCustomSearch
+{
+    /**
+     * Google Search Engine ID
+     *
+     * @var string
+     **/
+    protected $search_engine_id;
+
+    /**
+     * Google API Key
+     *
+     * @var string
+     **/
+    protected $api_key;
+
+    /**
+     * Constructor
+     *
+     * @param string search_engine_id Search Engine ID
+     * @param string api_key API Key
+     * @return void
+     **/
+    public function __construct($search_engine_id, $api_key)
+    {
+        $this->search_engine_id = $search_engine_id;
+        $this->api_key = $api_key;
+    }
+
+    /**
+     * Sends search request to Google
+     *
+     * @param array params The parameters of the search request
+     * @return object The raw results of the search request
+     **/
+    private function request($params) 
+    {
+        $params = array_merge(
+            $params,
+            [
+                'key' => $this->api_key,
+                'cx' => $this->search_engine_id
+            ]
+        );
+        
+        $context = stream_context_create([
+            'http' => [
+                'ignore_errors' => true
+            ]
+        ]);
+
+        return json_decode(
+            file_get_contents('https://www.googleapis.com/customsearch/v1?' . http_build_query($params), false, $context)
+        );
+    }
+
+    /**
+     * Perform search
+     *
+     * Returns an object with the following properties:
+     *
+     *   page
+     *   start
+     *   end
+     *   total_results
+     *   results
+     *     title
+     *     snippet
+     *     htmlSnippet
+     *     link
+     *     image
+     *     thumbnail
+     *
+     * @param string terms The search terms
+     * @param integer page The page to return
+     * @param integer per_page How many results to dispaly per page
+     * @param array extra Extra parameters to pass to Google
+     * @return object The results of the search
+     * @throws Exception If error is returned from Google
+     **/
+    public function search($terms, $page=1, $per_page=10, $extra=[])
+    {
+        // Google only allows 10 results at a time
+        $per_page = ($per_page > 10) ? 10 : $per_page;
+        
+        $params = [
+            'q' => $terms,
+            'start' => (($page - 1) * $per_page) + 1,
+            'num' => $per_page
+        ];
+        if (sizeof($extra)) {
+            $params = array_merge($params, $extra);
+        }
+
+        $response = $this->request($params);
+
+        if (isset($response->error)) {
+            throw new \Exception($response->error->message);
+        }
+        
+        $request_info = $response->queries->request[0];
+        
+        $results = new \stdClass();
+        $results->page = $page;
+        $results->start = $request_info->startIndex;
+        $results->end = ($request_info->startIndex + $request_info->count) - 1;
+        $results->total_results = $request_info->totalResults;
+        $results->results = [];
+
+        if (isset($response->items)) {
+            foreach ($response->items as $result) {
+                $results->results[] = (object) [
+                    'title' => $result->title,
+                    'snippet' => $result->snippet,
+                    'htmlSnippet' => $result->htmlSnippet,
+                    'link' => $result->link,
+                    'image' => isset($result->pagemap->cse_image) ? $result->pagemap->cse_image[0]->src : '',
+                    'thumbnail' => isset($result->pagemap->cse_thumbnail) ? $result->pagemap->cse_thumbnail[0]->src : '',
+                ];
+            }   
+        }
+        
+        return $results;
+    }
+}
